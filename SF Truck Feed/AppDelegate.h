5e970bf16d1563ec14110ecedd3072ac58bfc572
Richard Yeh <apocryphon.now@gmail.com>
2019-05-29 12:57:48 -0700

    Initial Commit

---
+++ SF Truck Feed/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SF Truck Feed
+//
+//  Created by Richard Yeh on 5/29/19.
+//  Copyright © 2019 Richard Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SF Truck Feed/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SF Truck Feed
+//
+//  Created by Richard Yeh on 5/29/19.
+//  Copyright © 2019 Richard Yeh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
